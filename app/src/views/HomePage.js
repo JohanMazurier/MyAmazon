145bf049c5d51614d73c4252ba099c15b4a8a211
Johan Mazurier <jmazurier@ema-o.com>
2021-07-09 01:57:08 +0200

    07 - redux homepage

--- app/src/views/HomePage.js
+++ app/src/views/HomePage.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, {useEffect} from 'react';
 import Product from "../component/Product";
 import Banner from "../component/Banner";
 import LoadingBox from "../component/LoadingBox";
 import MessageBox from "../component/MessageBox";
-
+import {useDispatch, useSelector} from 'react-redux'
+import {listProducts} from '../actions/productActions'
 
 import '../css/HomePage.css';
 import '../css/Product.css';
@@ -13,45 +13,31 @@ import data from "../data";
 
 
 function HomePage() {
-
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList)
+    const {loading, error, products} = productList;
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-
-                setLoading(true);
-
-                const { data } = await axios.get('/api/products');
-                setLoading(false);
-                setProducts(data);
-            } catch(err){
-                setError(err.message);
-                setLoading(false)
-            }
-        };
-        fetchData();
-    }, [])
+        dispatch(listProducts())
+    }, []);
     return (
         <div className="homePage">
             <div className="homeContainer">
-                <Banner />
+                <Banner/>
                 {loading ? (<LoadingBox></LoadingBox>)
-                : error ? (<MessageBox variant="danger">{error}</MessageBox>)
-                : (
-                    <div className="homeRow">
-                    {products.map((product) => (
-                        <Product
-                            key={product._id}
-                            product={product}
-                        >
-                        </Product>
-                    ))}
-                </div>
-                )}
-                
+                    : error ? (<MessageBox variant="danger">{error}</MessageBox>)
+                        : (
+                            <div className="homeRow">
+                                {products.map((product) => (
+                                    <Product
+                                        key={product._id}
+                                        product={product}
+                                    >
+                                    </Product>
+                                ))}
+                            </div>
+                        )}
+
             </div>
         </div>
     );
